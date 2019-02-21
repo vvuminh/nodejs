523e9ccf2dc327f2ea9c12e4394f89907209a324
vvuminh <minhtt262@gmail.com>
2019-02-21 10:14:39 +0700

    login

--- routes/users.js
+++ routes/users.js
@@ -33,11 +33,15 @@ router.post('/register', async (req, res) => {
       profileurl: profileURL,
       gender,
       dob,
+    },{
+      fields : ["name", "password", "email", "profileurl", "gender", "dob"]
     });
     if(newUser){
+      const token = jwt.sign({newUser}, 'my_secret_key');
       res.json({
         result : 'ok',
         data : newUser,
+        token : token,
         message : 'Insert a new user susseccfully'
       })
     }else {
@@ -48,6 +52,46 @@ router.post('/register', async (req, res) => {
     }
   } catch (error) {
     res.json({
+      error : `${error}`
+    })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  let {name, password} = req.body;
+  if(isEmpty(name) || isEmpty(password)){
+    res.json({
+      result : "false",
+      message : "nhap lai username or password"
+    });
+    return ;
+  }
+  try {
+    let users = await User.findAll({
+      attributes: ['id', 'name', 'password', 'email', 'profileurl', 'gender', 'dob'],
+      where: {
+        name,
+        password
+      }
+  });
+  if(users.length > 0) {
+    const token = jwt.sign({users}, 'my_secret_key');
+    res.json({
+        result: 'ok',
+        data: users,
+        token : token,
+        message: "login successfully"
+    });
+} else {
+    res.json({
+        result: 'failed',
+        data: {},
+        message: "user chua ton tai"
+    });
+}
+  } catch (error) {
+    res.json({
+      result : "dang nhap that bai",
       error : `${error}`
     })
   }
